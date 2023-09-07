a4899d01e2f3194e44d12fa8e0a3d7d518d4e840
hajat12 <144356276+hajat12@users.noreply.github.com>
2023-09-07 18:06:54 +0400

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['201098906252'] //ur owner number
-global.ownername = "ᗴᒪᘜᗩᘔᗩᖇ ᗩᒪᗯᗩᘔᗴᖇ❤️" //ur owner name
-global.ytname = "YT: ELGAZAR" //ur yt chanel name
-global.socialm = "GitHub: ELGAZAR" //ur github or insta name
-global.location = "Africa, Egypt, Cairo" //ur location
+global.owner = ['212771950043'] //ur owner number
+global.ownername = " anya Disney ❤️" //ur owner name
+global.ytname = "YT: anyaa" //ur yt chanel name
+global.socialm = "GitHub: anya" //ur github or insta name
+global.location = "Africa, morroco, Cairo" //ur location
 
 //bot bomdy 
-global.ownernomer = "201098906252" //ur number
-global.premium = ['201098906252'] //ur premium number
-global.botname = '❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️' //ur bot name
-global.linkz = "https://youtube.com/@ABDALLAH_MOHAMED" //your theme url which will be displayed on whatsapp
-global.websitex = "https://telegra.ph/file/ec763cc3e7c88d0e67682.jpg" //ur website to be displayed
-global.botscript = 'https://youtube.com/@ABDALLAH_MOHAMED' //script link
+global.ownernomer = "212771950043" //ur number
+global.premium = ['212771950043'] //ur premium number
+global.botname = 'desnie ᗷOT❤️' //ur bot name
+global.linkz = "" //your theme url which will be displayed on whatsapp
+global.websitex = "" //ur website to be displayed
+global.botscript = '' //script link
 global.themeemoji = "♥" //ur theme emoji
-global.packname = "⍣الجزار الوزير يروحي⍣" //ur sticker watermark packname
-global.author = "⍣01098906252⍣" //ur sticker watermark author
-global.wm = "❤️ᗴᒪᘜᗩᘔᗩᖇ ᗷOT❤️." //ur watermark
+global.packname = "⍣انيا دزني⍣" //ur sticker watermark packname
+global.author = "⍣+212771950043⍣" //ur sticker watermark author
+global.wm = "❤️desnie ᗷOT❤️." //ur watermark
 
 // Other
 global.sessionName = 'session'
